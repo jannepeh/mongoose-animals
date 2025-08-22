62ee23579a8b9fb2b5a376828cf22a2124a2ac96
jannepeh <jannepeh@metropolia.fi>
2025-08-22 10:38:08 +0300

    Latest additions for db connection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,19 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import CustomError from '../classes/CustomError';
 dotenv.config();
 
 const mongoConnect = async () => {
-  // Connect to MongoDB
+  try {
+    if (!process.env.DB_URL) {
+      throw new CustomError('Database url not found in .env file', 500);
+    }
+    const connection = await mongoose.connect(process.env.DB_URL);
+    console.log('DB connected successfully');
+    return connection;
+  } catch (error) {
+    console.error('Connection to db failed: ', (error as Error).message);
+  }
 };
 
 export default mongoConnect;
